5481167d0cb32e9d4a9be44d8a29bc9e3c5a9a0b
Kate Brown <kbrown8158@gmail.com>
2017-01-30 22:08:39 -0500

    added some tests, and formatting

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "person#{n}@example.com" }
+    first_name 'Libby'
+    last_name 'Levine'
+    password 'password'
+    password_confirmation 'password'
+    # admin false 
   end
 end
