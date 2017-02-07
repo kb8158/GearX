991589e813064b37b97b6e9c065c662506b62cb5
Kate Brown <kbrown8158@gmail.com>
2017-02-07 17:48:44 -0500

    added CRUD Function to react index page

--- react/src/components/ItemDisplay.js
+++ react/src/components/ItemDisplay.js
@@ -9,12 +9,24 @@ const ItemDisplay = props => {
     select = <form onSubmit={props.onSubmit}><button type="submit">Select This Item</button></form>
   }
 
+  let remove;
+  if(props.item.lender_id === props.currentUser.id){
+    remove = <form onSubmit={props.onDelete}><button type="submit">Delete This Item</button></form>
+  }
+
+  let update;
+  if(props.item.lender_id === props.currentUser.id){
+    update = <a href={`/items/${props.item.id}/edit`}>Update This Item</a>
+  }
+
   let itemShow = props => {
 
     if(props.selectedID === props.item.id) {
       itemDiv = <div className="item">{props.item.body}
                   <div>{props.item.size}</div>
                     {select}
+                    {remove}
+                    {update}
                   </div>
       return itemDiv;
     }
